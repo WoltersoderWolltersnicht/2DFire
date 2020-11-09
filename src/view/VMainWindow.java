c9d3db7addc5557696bf1076c6edebfd89d78dd4
alumno <alumno@f3306>
2020-11-09 14:48:28 +0100

    2

--- src/view/VMainWindow.java
+++ src/view/VMainWindow.java
@@ -32,6 +32,8 @@ public class VMainWindow extends JFrame {
 		panelControl.setLayout(null);
 		
 		slider = new JSlider();
+		slider.setValue(500);
+		slider.
 		slider.setMaximum(1000);
 		slider.setBounds(10, 11, 785, 26);
 		panelControl.add(slider);
