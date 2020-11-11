8a11ff77622c314ede367573d983155b283af51a
aaron <aaaro@DESKTOP-1T66D6E>
2020-11-11 22:49:26 +0100

    final v01

--- src/main.java
+++ src/main.java
@@ -2,6 +2,17 @@ import java.awt.EventQueue;
 
 import control.CMainWindow;
 
+/**
+*
+*Program that makes a frame with a variable Fire in it:
+*
+* @Version 1.0
+* @since 11/11/2020
+* 
+* @author Aaron Sanchez Wolters <aaaronwolters@gmail.com><aaronsanchez@estudiante.edib.es>
+* 
+*/
+
 public class main {
 
 	public static void main(String[] args) {
